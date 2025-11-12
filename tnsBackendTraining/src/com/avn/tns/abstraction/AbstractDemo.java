f5a3a75552e52e7a8c6287233861bf7448a4f77f
Admin-pc <Admin-pc@DESKTOP-RNGHL8A>
2025-11-12 22:47:40 +0530

    first commit

---
+++ tnsBackendTraining/src/com/avn/tns/abstraction/AbstractDemo.java
@@ -0,0 +1,11 @@
+package com.avn.tns.abstraction;
+//demo for abstract class
+//combination of abstract class + concrete
+abstract public class AbstractDemo {
+	abstract void add();
+	void display() {
+		System.out.println("this is from abstract");
+		
+	}
+
+}
---
+++ tnsBackendTraining/src/com/avn/tns/abstraction/AbstractMain.java
@@ -0,0 +1,13 @@
+package com.avn.tns.abstraction;
+
+public class AbstractMain {
+
+	public static void main(String[] args) {
+		
+		
+		AbstractDemo ob = new AbstractImpl ();
+		
+
+	}
+
+}
